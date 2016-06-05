984ba6f6fb0dd89964ee71f39cd8a0c3e08b11ef
jsw2060 <jsw2060@gmail.com>
2016-06-05 14:06:59 +0900

    0605

--- San/src/main/java/com/sist/mapred/NaverMapper.java
+++ San/src/main/java/com/sist/mapred/NaverMapper.java
@@ -56,3 +56,87 @@ public class NaverMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
 		}	
 	}
 }
+
+
+/*
+ * package com.sist.regex;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+
+^ 문자열의 시작    ^A       A%
+$ 문자열의 종료  T$       %T
+. 임의의 한 문자 (문자의 종류 가리지 않음)단, \ 는 넣을 수 없음
+* 앞 문자가 없을 수도 무한정 많을 수도 있음
++ 앞 문자가 하나 이상
+? 앞 문자가 없거나 하나있음
+[] 문자의 집합이나 범위를 나타내며 두 문자 사이는 - 기호로 범위를 나타낸다. 
+   [A-Z][a-z][0-9][가-핳]
+   []내에서 ^가 선행하여 존재하면 not 을 나타낸다.
+{} 횟수 또는 범위를 나타낸다.
+   ^A[a-z]{1,10}:뒤에 몇글자 오던지 상관안함
+   [0-9]{1,3}.[0-9]{1-3}.[0-9]{1-3}.[0-9]{1-3}       => IP주소
+() 소괄호 안의 문자를 하나의 문자로 인식 
+| 패턴 안에서 or 연산을 수행할 때 사용
+\s 공백 문자
+\S 공백 문자가 아닌 나머지 문자
+\w 알파벳이나 숫자
+\W 알파벳이나 숫자를 제외한 문자
+\d 숫자 [0-9]와 동일
+\D 숫자를 제외한 모든 문자
+\ 정규표현식 역슬래시(\)는 확장 문자
+ 
+ 
+public class RegExMapper extends Mapper<LongWritable, Text, Text, IntWritable>{      //한줄씩 읽어와서 단어별로 1씩 보내줌
+
+   private final IntWritable one=new IntWritable(1);
+   private Text result=new Text();
+   
+   //String regex="([0-9]{1,3})\\.([0-9]{1,3})\\.([0-9]{1,3})\\.([0-9]{1,3})";   //ip주소=java는 \\해야 \로 인식
+   String regex1="(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})";         //ip주소
+   
+    // 07/Mar/2004:16:23:12
+    
+   String regex2="\\d{2}/\\w{3}/\\d{4}";    //07/Mar/2004
+   String regex3="\\d{2}:\\d{2}:\\d{2}";   //16:23:12
+   
+   @Override
+   protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, IntWritable>.Context context)
+         throws IOException, InterruptedException {   //value=한줄씩 읽음
+      
+      Pattern p=Pattern.compile(regex3);         //ip주소 패턴
+      Matcher m=p.matcher(value.toString());      //한줄안에 ip주소가 있는가
+      while(m.find()){
+         result.set(m.group());      //패턴에 해당하는 실제 ip   //group(1)=> 그룹안에서 1번 단어묶음
+         context.write(result, one); //context에 누적해서 reducer로 넘김
+      }
+      
+   }
+   
+   
+   
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
